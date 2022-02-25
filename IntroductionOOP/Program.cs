7ec6f5b75c2d7f1a707cc62be41b53c6516335a6
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-02-26 00:44:51 +0400

    test push

--- IntroductionOOP/Program.cs
+++ IntroductionOOP/Program.cs
@@ -7,6 +7,8 @@ namespace IntroductionOOP
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World!");
         }
     }
 }
