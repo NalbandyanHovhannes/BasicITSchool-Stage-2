51122f5a6337d64bb447e156e16f01fe38832121
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-02-26 00:54:10 +0400

    adding examples

---
+++ 5-Generics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _5_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 6-DataStructures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _6_DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
