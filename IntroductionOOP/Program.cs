8e69dbd3d592f6e8264d329cf7cd7a0363754e57
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-03-13 16:49:45 +0400

    field incapsulation

--- IntroductionOOP/Program.cs
+++ IntroductionOOP/Program.cs
@@ -41,9 +41,58 @@ namespace IntroductionOOP
             //        Dispose();
             //    }
 
-            Console.WriteLine("Hello World!");
 
-            Console.WriteLine("Hello World!");
+            IncapsulationClass1 obj1 = new IncapsulationClass1();
+            //obj1.pubt = 8;
+            //obj1.t = 88;
+            IncapsulationClass2 obj2 = new IncapsulationClass2();
+            //obj2.pubt = 2;
+            //obj2.t = 22;
+            IncapsulationClass3 obj3 = new IncapsulationClass3();
+            //obj3.pubt = 3;
+            //obj3.t = 33;
+            IncapsulationClass4 obj4 = new IncapsulationClass4();
+            //obj4.pubt = 4;
+            //obj3.t = 44;
+            IncapsulationClass5 obj5 = new IncapsulationClass5();
+            //obj5.pubt = 55;
+            //obj5.t = 5;
+            IncapsulationClass6 obj6 = new IncapsulationClass6();
+            //obj6.pubt = 66;
+            //obj6.t = 6;
+
+        }
+
+        public class IncapsulationClass1
+        {
+            int t = 1;
+            public int pubt = 1;
+
+        }
+        public class IncapsulationClass2
+        {
+            public int t = 1;
+            public int pubt = 1;
+        }
+        public class IncapsulationClass3
+        {
+            protected int t = 1;
+            public int pubt = 1;
+        }
+        public class IncapsulationClass4
+        {
+            protected int t = 1;
+            public int pubt = 1;
+        }
+        public class IncapsulationClass5
+        {
+            internal int t = 1;
+            public int pubt = 1;
+        }
+        public class IncapsulationClass6
+        {
+            protected internal int t = 1;
+            public int pubt = 1;
         }
     }
 }
