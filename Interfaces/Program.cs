60c2279a72f34b113eb507ef253b43e9f493e9f7
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-03-15 00:38:15 +0400

    interface implementation

---
+++ Interfaces/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    interface Iimplementation
+    {
+        //public int t;
+
+        //property
+        public int prop1 { get; set; }
+
+        //propertyfull assign      
+
+        public int MyProperty
+        {
+            get { return prop1; }
+            set { prop1 = value; }
+        }
+
+        void Calc()
+        { }
+        class Testclass
+        {
+            public Testclass()
+            {
+
+            }
+        }
+      
+         void Getcv();
+
+
+    }
+
+    public class InitClass : Iimplementation
+    {
+       
+
+        public int prop1 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Getcv()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
