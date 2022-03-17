2983f47d24525ae828998bb554039f2930d08dbc
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-03-17 23:22:39 +0400

    nested2

---
+++ NestedAndSealedClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NestedClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class ParentClass
+    {
+        public int t;
+        public int MyProperty { get; set; }
+        public void Method1()
+        { Console.WriteLine("Method1"); }
+
+        public class NestedClass: Class2
+        {
+            public void Method2()
+            { Console.WriteLine("Method1"); }
+
+        }   
+       
+    }
+    public class Class2:ParentClass
+    {
+
+    }
+}
