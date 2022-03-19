e3c2e5bc463c2697ed7dfa87c874a4a70c8c2f22
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-03-19 12:48:10 +0400

    Linked lis hash set

--- 6-DataStructures/Program.cs
+++ 6-DataStructures/Program.cs
@@ -4,6 +4,7 @@ namespace _6_DataStructures
 {
     class Program
     {
+        array, list, linkedlist, stack, queue, dictionary
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
