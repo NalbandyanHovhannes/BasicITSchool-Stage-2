db7e391c8b231ecdd1c39d6c246d77c80efcab66
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-03-20 11:28:59 +0400

    List hash

--- 6-DataStructures/HashTable.cs
+++ 6-DataStructures/HashTable.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,21 @@ namespace _6_DataStructures
 {
     class HashTable
     {
+        public void HashMethod()
+        {
+            Hashtable HT = new Hashtable();
+            HT.Add(1, "s");
+            HT.Add(3, "n");
+            HT.Add(4, "j");
+            HT.Add(2, "a");
+            HT.Add(5, "u");
+            foreach (object i in HT.Keys)
+                Console.WriteLine(i);
+            foreach (object J in HT.Values)
+                Console.WriteLine(J);
+            foreach (DictionaryEntry di in HT)
+                Console.WriteLine("keys={0} values={1}", di.Key, di.Value);
+            Console.ReadKey();
+        }
     }
 }
--- 6-DataStructures/List.cs
+++ 6-DataStructures/List.cs
@@ -8,5 +8,68 @@ namespace _6_DataStructures
 {
     class List
     {
+        public void ListMethod()
+        {
+            List<int> numberList = new List<int>();
+            numberList.Add(32);
+            numberList.Add(21);
+            numberList.Add(45);
+            numberList.Add(11);
+            numberList.Add(89);
+            // List of string
+            List<string> authors = new List<string>(5);
+            authors.Add("Mahesh Chand");
+            authors.Add("Chris Love");
+            authors.Add("Allen O'neill");
+            authors.Add("Naveen Sharma");
+            authors.Add("Monica Rathbun");
+            authors.Add("David McCarter");
+
+            // Remove an item
+            authors.Remove("New Author1");
+
+            // Remove 3rd item
+            authors.RemoveAt(3);
+
+            // Remove a range
+            authors.RemoveRange(3, 2);
+
+            // Remove all items
+            authors.Clear();
+
+            Console.WriteLine(authors.IndexOf("Naveen Sharma", 2));
+
+            //reverse
+            authors.Reverse();
+
+
+
+            // Program: Copy items from one list to another list
+            Console.WriteLine("Import one list to another!");
+
+            // Create List1
+            List<string> listOne = new();
+            listOne.Add("One");
+            listOne.Add("Two");
+            listOne.Add("Three");
+            listOne.Add("Four");
+            listOne.Add("Five");
+
+            // Create List2
+            List<string> listTwo = new();
+            listTwo.Add("A");
+            listTwo.Add("B");
+            listTwo.Add("C");
+
+            // Add List2 to List1
+            listOne.AddRange(listTwo);
+
+            // Display
+            foreach (string item in listOne)
+                Console.WriteLine(item);
+
+            Console.ReadKey();
+
+        }
     }
 }
