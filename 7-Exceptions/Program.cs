bb398db103f5c1d8507a42e621f3b35c91899539
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-03-28 00:13:47 +0400

    exeption 4

--- 7-Exceptions/Program.cs
+++ 7-Exceptions/Program.cs
@@ -14,6 +14,33 @@ namespace _7_Exceptions
 
         public static void Main()
         {
+            try
+            {
+                int[] array = new int[2];
+                array[3] = 2;
+                var r=array[1]/int.Parse( Console.ReadLine());
+                Console.WriteLine("error in try block");
+                if (r>0)
+                {
+                    throw new Exception("its a Govnocode");
+                }
+
+            }
+            catch ( Exception ex)
+            {
+                Console.WriteLine("error in catch block"+ex.Message);
+              
+            }
+           
+            finally {
+                Console.WriteLine("error in finally block" );
+            }
+
+
+
+
+
+
 
             Testing1 test = new Testing1();
             test.Method1();
