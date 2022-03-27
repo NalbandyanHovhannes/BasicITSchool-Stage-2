077a1be21657074fa70dbb6e9c678f91fe7d3acc
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-03-27 23:06:35 +0400

    test2

---
+++ ExeptionTest2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ExeptionTest2
+{
+    class Program
+    {
+
+        public static string Method(string s)
+        {
+            if (!string.IsNullOrEmpty(s))
+            {
+                return s.Replace("k", "m");
+            }
+            else
+            {
+                throw new NullReferenceException("s is null");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int x;
+            try
+            {
+                x = 5;
+                Method(null);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e) when (e is AggregateException || e is ContextMarshalException) //C# 7 +
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (NullReferenceException e)
+            {
+                x = -1;
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
