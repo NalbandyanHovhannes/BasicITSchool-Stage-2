bce10f05b31645b1cf181820a765af3b07e5e56e
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-03-29 01:10:11 +0400

    files

--- 8-StreamsFilesDirectories/Program.cs
+++ 8-StreamsFilesDirectories/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;  // include the System.IO namespace
 
 namespace _8_StreamsFilesDirectories
 {
@@ -6,7 +7,30 @@ namespace _8_StreamsFilesDirectories
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FileMethod();
         }
+
+        //AppendText()    Appends text at the end of an existing file
+        //Copy()  Copies a file
+        //Delete()    Deletes a file
+        //Exists()    Tests whether the file exists
+        //ReadAllText()   Reads the contents of a file
+        //Replace()   Replaces the contents of a file with the contents of another file
+        //WriteAllText()  Creates a new file and writes the contents to it.
+        //If the file already exists, it will be overwritten.
+        public static void FileMethod()
+        {
+
+            string writeText = "Hello World!";  // Create a text string
+
+            //Create()    Creates or overwrites a file
+            File.Create("C:/Users/HP/Desktop/basic.txt");
+            File.WriteAllText("basic.txt", writeText);  // Create a file and write the content of writeText to it
+
+            string readText = File.ReadAllText("basic.txt");  // Read the contents of the file
+            Console.WriteLine(readText);  // Output the con
+
+        }
+
     }
 }
