9adc44378623f8abec8c91812dbdca9733f247f0
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-04-10 15:32:39 +0400

    thread

--- MultithreadingThreads/Program.cs
+++ MultithreadingThreads/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace MultithreadingThreads
 {
@@ -6,7 +8,11 @@ namespace MultithreadingThreads
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Thread thread = Thread.CurrentThread;
+            Console.WriteLine(thread.IsAlive);
+            Console.WriteLine(thread.Name);
+            Console.WriteLine(thread.GetHashCode());
+            Process.Start("https://www.facebook.com/");
         }
     }
 }
