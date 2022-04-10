c6ecfd1d6ac95cfac0b8f3998a17836691c31bf1
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-04-10 14:12:59 +0400

    Lambda

--- LambdaExpressionsReflectionAttributes/Lambda.cs
+++ LambdaExpressionsReflectionAttributes/Lambda.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace LambdaExpressionsReflectionAttributes
 {
+    public delegate double Calculate(float num);
+    public delegate void Print();
     public class Lambda
     {
         string test() { return "test"; }
         string test2() => "test2";
+        //------------------------------------
+        /// <summary>
+        /// explicit static method
+        /// </summary>
+        /// <param name="n">Number</param>
+        /// <returns></returns>
+        public static double Method(float n)
+        {
+            return n * n;
+        }
+
+        //public static double Method1(float n) => n* n;
 
     }
 }
--- LambdaExpressionsReflectionAttributes/Program.cs
+++ LambdaExpressionsReflectionAttributes/Program.cs
@@ -6,7 +6,34 @@ namespace LambdaExpressionsReflectionAttributes
     {
         static void Main(string[] args)
         {
+             static double Method(float n)
+            {
+                return n * n;
+            }
 
+            //Calculate calculate= new Calculate(Method);
+
+            Calculate calculate0 = Method; // Method explicit 
+
+            // Lambada
+            Calculate calculate1 = delegate (float n) { return n * n; };            // Method
+            Calculate calculate2 = (n) => { Console.WriteLine(); return n * n; };   // Operator
+            Calculate calculate3 = (n) => n * n;                                    // Expression
+
+            Print p = () => Console.WriteLine();
+            //calculate1=calculate1=calculate1 
+
+
+
+
+
+
+
+
+
+
+
+            //Attribute
             var photo = new Photo("Screenshot_2.png")
             { Path = @":\Users\HP\Desktop" };
 
