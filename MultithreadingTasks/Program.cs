bfa2a93f7d47616439db9be551148d8bb8bba4ef
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-04-10 16:27:57 +0400

    task

--- MultithreadingTasks/Program.cs
+++ MultithreadingTasks/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultithreadingTasks
 {
@@ -7,7 +9,32 @@ namespace MultithreadingTasks
         private int x = 1;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int i = 0;
+            async void Method2()
+            {
+                int i = 100000;
+                while (i != 100100)
+                {
+                    i++;
+                    //await Thread.Sleep(500);
+                    Console.WriteLine(i);
+                }
+               
+            }
+            async void Method1()
+            {
+                await Task.Run(() =>
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        Console.WriteLine(i);
+
+                    }
+                });
+            }
+            Method2();
+            Method1();
+            Console.WriteLine("ha inch anem");
         }
     }
 }
