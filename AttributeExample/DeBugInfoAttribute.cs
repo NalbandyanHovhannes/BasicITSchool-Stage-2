3b048ccb11a9bb3a1a4a35f1d83f5bb1b575b379
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-04-11 00:04:39 +0400

    atributes

---
+++ AttributeExample/DeBugInfoAttribute.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace AttributeExample
+{
+    [AttributeUsage(
+   AttributeTargets.Class |
+   AttributeTargets.Constructor |
+   AttributeTargets.Field |
+   AttributeTargets.Method |
+   AttributeTargets.Property,
+   AllowMultiple = true)]
+
+    public class DeBugInfoAttribute : System.Attribute
+    {
+        private int bugNo;
+        private string developer;
+        private string lastReview;
+        public string message;
+
+        public DeBugInfoAttribute(int bg, string dev, string d)
+        {
+            this.bugNo = bg;
+            this.developer = dev;
+            this.lastReview = d;
+        }
+        public int BugNo
+        {
+            get
+            {
+                return bugNo;
+            }
+        }
+        public string Developer
+        {
+            get
+            {
+                return developer;
+            }
+        }
+        public string LastReview
+        {
+            get
+            {
+                return lastReview;
+            }
+        }
+        public string Message
+        {
+            get
+            {
+                return message;
+            }
+            set
+            {
+                message = value;
+            }
+        }
+    }
+}
