b7c64844f1dccd8a8d53f5ee802a1b2e9fea5d25
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-04-13 16:28:06 +0400

    tasks

--- MultithreadingTasks/Program.cs
+++ MultithreadingTasks/Program.cs
@@ -6,6 +6,30 @@ namespace MultithreadingTasks
 {
     class Program
     {
+
+        //Task task = new Task(() => Console.WriteLine("Hello Task!"));
+        //task.Start();
+        //Task task = Task.Factory.StartNew(() => Console.WriteLine("Hello Task!"));
+        //Task task = Task.Run(() => Console.WriteLine("Hello Task!"));
+
+        //Task task1 = new Task(() => Console.WriteLine("Task1 is executed"));
+        //task1.Start();
+
+        //Task task2 = Task.Factory.StartNew(() => Console.WriteLine("Task2 is executed"));
+
+        //Task task3 = Task.Run(() => Console.WriteLine("Task3 is executed"));
+
+        //task1.Wait();   // wait end of task1
+        //task2.Wait();   // wait end of task2
+        //task3.Wait();   // wait end of task3
+
+
+        //task1.Start();  
+        //task1.Wait();   
+
+       // task1.RunSynchronously();  start sync
+
+
         private int x = 1;
         static void Main(string[] args)
         {
