6fb6f321a1fc60425520c4d633a5e7c1cc157e0d
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-10-14 22:02:14 +0400

    Records and lambda expressions.

---
+++ LambdaExample/Program.cs
@@ -0,0 +1,11 @@
+﻿List<string> Names = new List<string>() { "Khachik", "Vachik", 
+                                                "Qajik", "Gurgen", "Davit"};
+
+var newList = Names.FindAll(x => x.ToLower().EndsWith("ik") 
+                                || x.StartsWith("D") 
+                                   && x.Length > 5);
+foreach(var name in newList)
+{
+    Console.WriteLine(name);
+}
+
---
+++ RecordExample1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Person a = new Person("Gor");
+Person b = new Person("Gor");
+Console.WriteLine("ReferenceEquals(a, b) = {0}",
+                                Object.ReferenceEquals(a, b));
+string c = "a";
+string d = "a";
+Console.WriteLine("ReferenceEquals(c, d) = {0}",
+                                Object.ReferenceEquals(c, d));
+Console.WriteLine(a==b);
+Console.WriteLine(a);
+record class Person(string Name);
+
