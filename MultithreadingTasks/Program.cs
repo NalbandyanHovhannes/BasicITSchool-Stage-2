6d8f6bdc99c9f8a1b38fcbfa0050e44859a75d70
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-10-25 21:27:39 +0400

    tasks

--- MultithreadingTasks/Program.cs
+++ MultithreadingTasks/Program.cs
@@ -9,14 +9,28 @@ namespace MultithreadingTasks
         static  void Main(string[] args)
            
         {
-            //Method1();
+           // Method1();
             Method2();
-            //Console.WriteLine("3");
+           
             Console.ReadKey();            
         }
 
         static void Method2()
+
+
         {
+            //Task task1 = new Task(() => Console.WriteLine("Hello Task111111111111!"));
+            //Task task2 = new Task(() => Console.WriteLine("Hello 2222222222222222222!"));
+            //Task task3 = new Task(() => Console.WriteLine("Hello 33333333333333!"));
+            Method1async("Task111111111111");
+            Method1async("33333333333333");
+            //task1.Wait(100);   // wait end of task1
+            //task2.Wait(300);   // wait end of task2
+            //task3.Wait(900);   // wait end of task3
+            //task1.Start();
+            //task2.Start();
+            //task3.Start();
+            //Console.ReadKey();
             //Task task5 = new Task(() => Thread.Sleep(2000000000)); 
 
             //Task task2 = Task.Factory.StartNew(() => Console.WriteLine("Hello Task222222222222!"));
@@ -25,38 +39,25 @@ namespace MultithreadingTasks
             //task1.Start();
 
 
-            Task task1 = new Task(() => Console.WriteLine("Hello Task111111111111!"));
-            Task task2 = new Task(() => Console.WriteLine("Hello 2222222222222222222!"));
-            Task task3 = new Task(() => Console.WriteLine("Hello 33333333333333!"));
-
-
             //Task task2 = Task.Factory.StartNew(() => Console.WriteLine("Task2 is executed"));
             //Task task3 = Task.Run(() => Console.WriteLine("Task3 is executed"));
-            task1.Wait(100);   // wait end of task1
-            task2.Wait(300);   // wait end of task2
-            task3.Wait(900);   // wait end of task3
-            task1.Start();
-            task2.Start();
-            task3.Start();
-            Console.ReadKey();
             //task2.Start();
             //task3.Start();
             //task1.Wait(2000);
             //task1.RunSynchronously();
         }
-        static async void Method1async()
+        static  void Method1async(string t)
         {
-            Task.Run(() =>
-           {
-               DoWork1();
-           });
-            Console.ReadKey();
+        
+               DoWork1(t);
+      
+          
         }
-        public static void DoWork1()
+        public static void DoWork1(string n)
         {
-            for (int t = 0; t < 10; t++)
+            for (int t = 0; t < 2000; t++)
             {
-                Console.WriteLine("1");
+                Console.WriteLine(n);
             }
         }
         public static void DoWork2()
